43e3732a1b69da386f31de6cbf731419798abe34
andrei.pop <andrei.pop@softvision.com>
2021-06-28 12:31:00 +0300

    Second commit Andrei.Pop

---
+++ Andrei.Pop_Directory/first_file.py
@@ -0,0 +1,46 @@
+print ('Hello World!!!')
+
+print('*******************************')
+a = [1, 2, 1]
+b = set(a)
+print(b)
+
+print('*******************************')
+c = list(range(0, 10, 2))
+print('c=', c)
+
+print('*******************************')
+x = 300
+y = 300
+
+print(id(x))
+print(id(y))
+
+print('*******************************')
+print(type(x))
+print(type(c))
+print(type(b))
+
+print('*******************************')
+s = 3
+t = float(s)
+print(s)
+print(type(s))
+print(t)
+print(type(t))
+
+print('*******************************')
+f = False
+i = True
+print(type(f))
+print(type(i))
+
+print('*******************************')
+print(type(f) == type(t))
+
+print(f == False)
+
+print('*******************************')
+a = '1234'
+print(len(a))
+
--- Andrei.Pop_Directory/second_file.py
+++
@@ -1,2 +0,0 @@
-val = input("Give me a value: ")
-print(f'Val has value {val}')
