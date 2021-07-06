2fc111584ce62c52f429dbe3cb008b11cf71b754
andrei.pop <andrei.pop@softvision.com>
2021-07-06 10:00:41 +0300

    Exercises Module 3

---
+++ Module 3/Conditionals_part2.py
@@ -0,0 +1,106 @@
+print('                ')
+
+print('      EX1       ')
+
+# Iterables iter()
+
+# print(iter('string')) # string
+#
+# print(iter(['mod1', 'mod2', 'mod3']))  # list
+#
+# print(iter(('tuple1', 'tuple2', 'tuple3')))  # tuple
+#
+# print(iter({'set1', 'set2', 'set3'}))  # set
+#
+# print(iter({'aaa': 1, 'bbb': 2, 'ccc': 3}))  # dict
+#
+
+print('                ')
+
+print('      EX2       ')
+
+# print(iter(112))  # int
+
+# print(iter(2,47))  # float
+
+# print(iter(len))  # built-in function
+
+print('    For over list    ')
+
+print('                ')
+
+print('      EX3       ')
+
+fruits = ('apples', 'banana', 'cherry')
+brake_fruits = ('portocala', 'pepene', 'croampe')
+
+# for i in fruits:
+#     print(i)
+#
+# for i in 'str':
+#     print(i)
+#
+# for i in brake_fruits:
+#     print(i)
+#     if i =="croampe":
+#         break
+
+# for i in brake_fruits:
+#     if i == 'pepene':
+#         break
+#     print(i)
+
+
+print('      For over dict          ')
+
+print('     Ex4        ')
+
+print('                ')
+
+dictionar = {'for': 1, 'if': 2, 'else': 3}
+
+# for k in dictionar.keys():
+#     print(k)
+#
+# for k in dictionar:
+#     print(k)
+#
+# for k in dictionar.values():
+#     print(k)
+#
+# for k in dictionar:
+#     print(dictionar[k])
+
+# i, j = (1, 2)
+# print(i,j)
+#
+# for i, j in [(1,2), (3,4), (5,6),(7,8)]:
+# #     print(i, j)
+#
+# print(dictionar.items())
+#
+# for k,v in dictionar.items():
+#     print('key =',k,'value ='. v)
+
+print('     Range (start stop step)          ')
+
+print('     EX 5        ')
+
+print('                ')
+#
+# my_range = range(3,6)
+
+# for i in my_range:
+#     print(i)
+#
+# my_range = range(3, 21)
+#
+# for i in my_range:
+#     print(i)
+#
+# for n in (1, 2, 3, 4, 5, 6, 7, 8):
+#     print(n)
+my_range = range(5)
+# for i in my_range:
+#     print(i)
+
