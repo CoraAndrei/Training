818ec74bcbd17648830248bb5848acc18e677807
andrei.gogoneata <andrei.gogoneata@softvision.com>
2021-07-09 19:08:47 +0300

    Exercices_Part3.py

---
+++ Module 3/Exercices_Part3.py
@@ -0,0 +1,30 @@
+# # Exercise 1 :
+# # Write a while loop that adds all the numbers up to 10
+# # Hint:
+#
+# counter = 0
+# total = 0
+#
+# # Exercise 2 :
+# # Using while loop, if statement and str() function; iterate through the list and if there is a 100,
+# # print it with its index number. i.e.:
+# # "There is a 100 at index no: 5"
+#
+# lst = [10, 99, 98, 85, 45, 59, 65, 66, 76, 12, 35, 13, 100, 80, 95]
+
+#
+# # Exercise 3 :
+# # Display the numbers from 1 to 10 except 6.
+# # Expected Output: 1 2 3 4 5 7 8 9 10
+#
+# # Exercise 4 :
+# # Use .pop to print the given list backwards
+
+klass = ['Oana', 'Andreea', 'Lidia', 'Daniel', 'SRJ', 'Andrei P.', 'Andrei C.', 'Gogo']
+
+# # Exercise 4.1:
+# # Use .pop to the same list given at Ex5 and create a while loop to see how is paying attention in Class or not
+
+# # Exercise 5:
+# #  Using the same list as the list from the ex5 check if a certain person is 'in_class_or_not'
+# # Hint: Use 'in' in order to check
