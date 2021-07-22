30484fad0c4d5e926a708b9e491c754526e8a79e
lidia.bejerea <lidiabejerea@gmail.com>
2021-07-22 18:19:10 +0300

    Homework results

---
+++ HomeworkResults/Lidia_Homework_M3_Part3.py
@@ -0,0 +1,14 @@
+# # Exercise 1 :
+# # Write a while loop that adds all the numbers up to 10
+
+n = 0
+while n <= 10:
+    print(n)
+    n += 1
+
+# # Exercise 2 :
+# # Using while loop, if statement and str() function; iterate through the list and if there is a 100,
+# # print it with its index number. i.e.:
+# # "There is a 100 at index no: 5"
+#
+lst = [10, 99, 98, 85, 45, 59, 65, 66, 76, 12, 35, 13, 100, 80, 95]
